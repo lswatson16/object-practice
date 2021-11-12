c4689d1a18df0fbb5a02c95b9bfe962b74ac15e6
Lauralyn Watson <lauralynw16@gmail.com>
2021-11-12 17:27:55 -0500

    Add object variable customer

--- object-examples.js
+++ object-examples.js
@@ -6,11 +6,18 @@ var car = {
   doorCount: 4,
   trim: "leather"
 };
-console.log(car);
+// console.log(car);
 
 var animal = {
   hasFur: true,
   numLegs: 4,
   favoriteFoods: ["fish", "shrimp"]
 };
-console.log(animal);
+// console.log(animal);
+
+var customer = {
+  names: ["Ava", "Bob", "Charles"],
+  number: 1890,
+  likesProduct: true
+}
+// console.log(customer);
